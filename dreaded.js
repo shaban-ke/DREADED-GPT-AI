5e226f572cf1fd3b7d0f9ebaef5732bb8ab47fb4
shaban-ke <154048741+shaban-ke@users.noreply.github.com>
2023-12-22 02:17:29 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -449,7 +449,7 @@ if (antilink === 'TRUE' && antilinkall === 'TRUE' && body.includes('http') && !O
       switch (command) {
       case "help":
         case "menu":
-let cap = `┌───═[ TRIPPKID ]═──▸
+let cap = `┌───═[ 𝕺𝖋𝖋𝖎𝖈𝖎𝖆𝖑 6𝖜𝖟𝖉𝖇𝖗 ]═──▸
 
 ▯ ʜᴇʟʟᴏ ɪᴛ'ꜱ (ᴅʀᴇᴀᴅᴇᴅ ʙᴏᴛ) ᴛʜɪꜱ ɪꜱ ᴀ ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. ʙᴇʟᴏᴡ ɪꜱ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ.
 ▯
