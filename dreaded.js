584579389ebf2c26e9e79208c6644b60379f75a7
shaban-ke <154048741+shaban-ke@users.noreply.github.com>
2024-01-11 01:03:43 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -449,7 +449,7 @@ if (antilink === 'TRUE' && antilinkall === 'TRUE' && body.includes('http') && !O
       switch (command) {
       case "help":
         case "menu":
-let cap = `┌───═[ 🅖︎🅡︎🅞︎🅖︎🅤︎ the 𓃵 ]═──▸
+let cap = `┌───═[ iT's TED ]═──▸
 
 ▯ ʜᴇʟʟᴏ ɪᴛ'ꜱ (ᴅʀᴇᴀᴅᴇᴅ ʙᴏᴛ) ᴛʜɪꜱ ɪꜱ ᴀ ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. ʙᴇʟᴏᴡ ɪꜱ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ.
 ▯
