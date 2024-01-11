984f539e683b32dd208c674b75a001ef3708555f
shaban-ke <154048741+shaban-ke@users.noreply.github.com>
2024-01-11 13:59:50 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -449,7 +449,7 @@ if (antilink === 'TRUE' && antilinkall === 'TRUE' && body.includes('http') && !O
       switch (command) {
       case "help":
         case "menu":
-let cap = `┌───═[ Gnarlyreynolds ]═──▸
+let cap = `┌───═[ MonicQ Ventures ]═──▸
 
 ▯ ʜᴇʟʟᴏ ɪᴛ'ꜱ (ᴅʀᴇᴀᴅᴇᴅ ʙᴏᴛ) ᴛʜɪꜱ ɪꜱ ᴀ ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. ʙᴇʟᴏᴡ ɪꜱ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ.
 ▯
