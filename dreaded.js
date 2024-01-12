9d8b7893172ade6d4082f87baad63ff4e85a7e8d
shaban-ke <154048741+shaban-ke@users.noreply.github.com>
2024-01-12 23:22:55 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -449,7 +449,7 @@ if (antilink === 'TRUE' && antilinkall === 'TRUE' && body.includes('http') && !O
       switch (command) {
       case "help":
         case "menu":
-let cap = `┌───═[ ★𝕶𝖊𝖑𝖛𝖎𝖓👑 ]═──▸
+let cap = `┌───═[ ⚝KAMENE⚝ ]═──▸
 
 ▯ ʜᴇʟʟᴏ ɪᴛ'ꜱ (ᴅʀᴇᴀᴅᴇᴅ ʙᴏᴛ) ᴛʜɪꜱ ɪꜱ ᴀ ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. ʙᴇʟᴏᴡ ɪꜱ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ.
 ▯
